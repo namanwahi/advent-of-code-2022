7d220fd96e032998ac70139db64553dcdaae130f
Naman Wahi <namanwahi@yahoo.co.uk>
2023-03-26 14:40:54 +0100

    day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,8 +1,9 @@
 module Day1 (solution) where
+import Data.List
 
 solution :: [String] -> Int
-solution = maximum . foldl addCalories [0]
+solution = sum . take 3. reverse . sort . foldl addCalories [0]
     where
         addCalories:: [Int] -> String -> [Int]
         addCalories calorieSums "" = 0 : calorieSums
-        addCalories (currenSum: calorieSums) calStr = (currenSum + read calStr) : calorieSums 
+        addCalories (currenSum: calorieSums) calStr = (currenSum + read calStr) : calorieSums
